a340084047866af2b4a48c588dbace16faf8058b
zelenchuk <zelenchyks@gmail.com>
2024-01-02 08:37:51 -0500

    Started using the `PAGINATION_LIMIT` .env variable for manage default pagination limit.

--- internal/mdw/pagination.go
+++ internal/mdw/pagination.go
@@ -1,6 +1,7 @@
 package mdw
 
 import (
+	"os"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
@@ -12,13 +13,27 @@ type PaginationInfo struct {
 	Offset int
 }
 
+func getPaginationLimit() int {
+	limitStr := os.Getenv("PAGINATION_LIMIT")
+	if limitStr == "" {
+		return 10
+	}
+
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil {
+		return 10
+	}
+
+	return limit
+}
+
 func PaginationMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		page, _ := strconv.Atoi(c.QueryParam("page"))
 		if page == 0 {
 			page = 1
 		}
-		limit := 10 // todo - make this configurable
+		limit := getPaginationLimit()
 
 		pagination := &PaginationInfo{
 			Page:   page,
